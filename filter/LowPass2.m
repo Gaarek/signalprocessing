0dc0a8646bd0c40573ccb226f6f3e9c8d399eb36
Mathias Persson <matpe313@hotmail.com>
2020-01-05 19:50:16 +0100

    Change for handling varying time steps. Added help text

--- filter/LowPass2.m
+++ filter/LowPass2.m
@@ -1,6 +1,17 @@
 function [y, state] = LowPass2(varargin)
-%LOWPASS Summary of this function goes here
-%   Detailed explanation goes here
+%LOWPASS2 Second order low-pass filter
+%   Initialize with:
+%     [~ state] = LowPass2(w, z)
+%   Filter with:
+%     [y, state] = LowPass2(x, time, state)
+%
+%   w: Bandwidth [rad/s]
+%   z: Damping coefficient, typically 0.5-1.2
+%   state: Internal state of the filter, input and output should be the
+%          same variable
+%   x: Input value to filter
+%   time: Time stamp of x
+%   y: Filtered output
 
 if (nargin == 3)
     x = varargin{1};
@@ -11,26 +22,37 @@ if (nargin == 3)
         %This handles changing time step, but not really.
         %It is still assumed that the tim step between n -> n-1 -> n-2 is
         %constant, which it doesnt have to be.
-        %Kind of work, but needs further work.
-        %Handled now by interpolating xm2/ym2 to time - 2*dt
-        %Can still create strange results when time steps vary alot.
-        %Especially when the time step suddenly increases.
+        %Kind of works, but needs further work.
+        %Handled now by interpolation
+        
         dt = time - state.oldTime;
-        dt2inv = 1/(state.oldTime - state.oldOldTime);
-        w = state.w;
+        dtOld = state.oldTime - state.oldOldTime;
+        
+        %Move either n-1 or n-2 to make time steps uniform and dont
+        %extrapolate (new points between oldOldTime and time)
+        if (dt <= dtOld)
+            dtOldinv = 1/dtOld;
+            xm1 = state.xm1;
+            ym1 = state.ym1;
+            xm2 = state.xm1 - dt * (state.xm1 - state.xm2)*dtOldinv;
+            ym2 = state.ym1 - dt * (state.ym1 - state.ym2)*dtOldinv;
+        else
+            xm2 = state.xm2;
+            ym2 = state.ym2;
+            xm1 = x - dtOld * (x - state.xm1)/dt;
+            ym1 = state.ym2 + (dt + dtOld)/2 * (state.ym1 - state.ym2)/dtOld;
+        end
+        w = state.w/1.4;
         K=w/tan(w*dt/2);
         %K=2/dt;
         
-        num = state.a0*K^2 + state.a1*K + state.a2;
-        
-        xm2 = state.xm1 - dt * (state.xm1 - state.xm2)*dt2inv;
-        ym2 = state.ym1 - dt * (state.ym1 - state.ym2)*dt2inv;
+        num = state.a0*K^2 + state.a1*K + 1;
         
         y = x +...
-            (2*1)*state.xm1 +...
-            1*xm2 +...
-            -(2 * state.a2 - 2*state.a0 * K^2) * state.ym1 +...
-            -(state.a0*K^2 - state.a1*K + state.a2)*ym2;
+            (2 * 1) * xm1 +...
+            1 * xm2 +...
+            -(2 * 1 - 2 * state.a0 * K^2) * ym1 +...
+            -(state.a0 * K^2 - state.a1 * K + 1) * ym2;
         y = y/num;
         
         state.ym2 = state.ym1;
@@ -43,7 +65,7 @@ if (nargin == 3)
         state.ym2 = y;
         state.xm1 = x;
         state.xm2 = x;
-        state.firstSample = false;        
+        state.firstSample = false;
         state.oldTime = time - 1;
     end
     state.oldOldTime = state.oldTime;
@@ -55,6 +77,5 @@ elseif (nargin == 2)
     state.z = varargin{2};
     state.a0 = 1/state.w^2;
     state.a1 = 2*state.z/state.w;
-    state.a2 = 1;
     y = 0;
 end
--- filter/test.m
+++ filter/test.m
@@ -5,24 +5,28 @@ stepTime = 0;
 
 figure(1);
 hold off;
-for i=[0, 2, 20]
+for i=[0, 2, 30]
     [~, state] = LowPass2(3, 0.707);
+    xl=[];
     yl=[];
+    
     ytl=[];
     t=min(tl);
-    while t<= max(tl)        
+    while t<= max(tl)
         if (t < stepTime)
             x=0;
         else
             x=1;
         end
+        %         x=x+0.1*randn(1,1);
         [y, state] = LowPass2(x, t, state);
+        xl(end+1) = x;
         yl(end+1) = y;
         ytl(end+1) = t;
         
-        t = t + dt *( 1 + i * rand(1,1)); 
+        t = t + dt * (1 + i * rand(1,1));
     end
-    
-    plot(ytl, yl)
+    plot(ytl, xl, 'LineWidth', 1);
     hold all;
+    plot(ytl, yl)
 end
